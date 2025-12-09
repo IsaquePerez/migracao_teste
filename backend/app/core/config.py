403d8157e254fc0e01137d84673f756ed95fbb7b
IsaquePerez <isaquenovo127@gmail.com>
2025-12-09 14:40:33 -0300

    teste

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,14 +4,14 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8', extra='ignore')
 
     # Variáveis do .env
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int
-    POSTGRES_DB: str
+    POSTGRES_USER: str | None = None
+    POSTGRES_PASSWORD: str | None = None
+    POSTGRES_HOST: str | None = None
+    POSTGRES_PORT: int | None = None
+    POSTGRES_DB: str | None = None
     DATABASE_URL: str | None = None
     SECRET_KEY: str
-    ALGORITHM: str = "HS256"
+    ALGORITHM: str = "HS256" 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     @property
